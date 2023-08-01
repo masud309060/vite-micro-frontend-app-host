e68ee248fd6724e00f2a6f280c374d15e911140a
masud-orangetoolz <masud.rana@orangetoolz.com>
2023-08-02 00:04:54 +0600

    feat: lazy load issue solved

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ import Externals from "./components/core/layouts/Externals.tsx";
 const PublicPage = React.lazy(() => import('./components/Public/PublicPage.tsx'));
 const CountButton = React.lazy(() => import("remoteAppOne/CountButton"));
 const Dashboard = React.lazy(() => import("dashboard/Dashboard"))
+const DashboardGraph = React.lazy(() => import("dashboard/DashboardGraph"))
 const Contact = React.lazy(() => import("contact/Contact"));
+const ContactDetails = React.lazy(() => import("contact/ContactDetails"));
 
 const router = createBrowserRouter([
     {
@@ -22,8 +24,16 @@ const router = createBrowserRouter([
                 element: <Externals><Dashboard/></Externals>,
             },
             {
+                path: "/dashboard/graph",
+                element: <Externals><DashboardGraph/></Externals>,
+            },
+            {
                 path: "/contact",
                 element: <Externals><Contact/></Externals>,
+            },
+            {
+                path: "/contact/:id",
+                element: <Externals><ContactDetails/></Externals>,
             },
             {
                 path: "/button",
--- src/remoteModule.d.ts
+++ src/remoteModule.d.ts
@@ -1,5 +1,7 @@
 declare module "remoteAppOne/CountButton";
 declare module "remoteAppOne/store";
 declare module "dashboard/Dashboard";
+declare module "dashboard/DashboardGraph";
 declare module "contact/Contact";
+declare module "contact/ContactDetails";
 
